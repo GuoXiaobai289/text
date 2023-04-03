9ba483fb0ff4bc3f4e7497192561b56b6841bdd6
Gsheng <2925057702@qq.com>
2023-04-03 16:21:42 +0800

    dd

--- src/main/java/cn/iurac/testsystem/TextSystemMain.java
+++ src/main/java/cn/iurac/testsystem/TextSystemMain.java
@@ -1,10 +1,8 @@
 package cn.iurac.testsystem;
 
-import cn.hutool.core.date.DateUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,9 +12,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2 // 开启 swagger
 @EnableScheduling // 开启定时任务功能
 @EnableAspectJAutoProxy(proxyTargetClass=true)
-public class Main {
+public class TextSystemMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class,args);
+        SpringApplication.run(TextSystemMain.class,args);
     }
 }
